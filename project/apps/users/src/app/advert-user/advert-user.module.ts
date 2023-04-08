ff6baed63fb844c4e411d23e88c50ded7a015a3c
gladosq <gladosq@gmail.com>
2023-04-08 19:11:55 +0400

    added mongodb

--- project/apps/users/src/app/advert-user/advert-user.module.ts
+++ project/apps/users/src/app/advert-user/advert-user.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {AdvertUserMemoryRepository} from './advert-user-memory.repository';
+import {MongooseModule} from '@nestjs/mongoose';
+import {AdvertUserModel, AdvertUserSchema} from './advert-user.model';
+import {AdvertUserRepository} from './advert-user.repository';
 
 @Module({
-  providers: [AdvertUserMemoryRepository],
-  exports: [AdvertUserMemoryRepository]
+  imports: [MongooseModule.forFeature([
+    {name: AdvertUserModel.name, schema: AdvertUserSchema}
+  ])],
+  providers: [AdvertUserRepository],
+  exports: [AdvertUserRepository]
 })
-export class AdvertUserModule {}
+export class AdvertUserModule {
+}
--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,4 +1,4 @@
-import {ConflictException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
+import {ConflictException, Inject, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
 import {AdvertUserMemoryRepository} from '../advert-user/advert-user-memory.repository';
 import {CreateUserDto} from './dto/create-user.dto';
 import {UserRole} from '@project/shared/app-types';
@@ -6,15 +6,30 @@ import dayjs from 'dayjs';
 import {AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG} from './authentication.constant';
 import {AdvertUserEntity} from '../advert-user/advert-user.entity';
 import {LoginUserDto} from './dto/login-user.dto';
+import {dbConfig} from '@project/config/config-users';
+import {ConfigService, ConfigType} from '@nestjs/config';
+import {AdvertUserRepository} from '../advert-user/advert-user.repository';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
-    private readonly advertUserRepository: AdvertUserMemoryRepository
-  ) {}
+    private readonly advertUserRepository: AdvertUserRepository,
+    private readonly configService: ConfigService,
+
+    @Inject(dbConfig.KEY)
+    private readonly databaseConfig: ConfigType<typeof dbConfig>,
+  ) {
+    // Извлекаем настройки из конфигурации. Вариант 1
+    console.log(databaseConfig.host);
+    console.log(databaseConfig.user);
+
+    // Извлекаем настройки из конфигурации. Вариант 2
+    console.log(configService.get<string>('db.host'));
+    console.log(configService.get<string>('db.user'));
+  }
 
   public async register(dto: CreateUserDto) {
-    const {email, name, password, dateBirth} = dto;
+    const {email, name, password, dateBirth, city} = dto;
 
     const advertUser = {
       email,
@@ -23,7 +38,7 @@ export class AuthenticationService {
       avatar: '',
       dateBirth: dayjs(dateBirth).toDate(),
       passwordHash: '',
-      city: ''
+      city
     };
 
     const existUser = await this.advertUserRepository
