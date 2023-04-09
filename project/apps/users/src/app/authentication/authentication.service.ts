d5516e0088dd6888612d4c928f89ca65aac3df39
gladosq <gladosq@gmail.com>
2023-04-09 16:43:05 +0400

    fixes

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,32 +1,17 @@
 import {ConflictException, Inject, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
-import {AdvertUserMemoryRepository} from '../advert-user/advert-user-memory.repository';
 import {CreateUserDto} from './dto/create-user.dto';
 import {UserRole} from '@project/shared/app-types';
 import dayjs from 'dayjs';
 import {AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG} from './authentication.constant';
 import {AdvertUserEntity} from '../advert-user/advert-user.entity';
 import {LoginUserDto} from './dto/login-user.dto';
-import {dbConfig} from '@project/config/config-users';
-import {ConfigService, ConfigType} from '@nestjs/config';
 import {AdvertUserRepository} from '../advert-user/advert-user.repository';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly advertUserRepository: AdvertUserRepository,
-    private readonly configService: ConfigService,
-
-    @Inject(dbConfig.KEY)
-    private readonly databaseConfig: ConfigType<typeof dbConfig>,
-  ) {
-    // Извлекаем настройки из конфигурации. Вариант 1
-    console.log(databaseConfig.host);
-    console.log(databaseConfig.user);
-
-    // Извлекаем настройки из конфигурации. Вариант 2
-    console.log(configService.get<string>('db.host'));
-    console.log(configService.get<string>('db.user'));
-  }
+  ) {}
 
   public async register(dto: CreateUserDto) {
     const {email, name, password, dateBirth, city} = dto;
